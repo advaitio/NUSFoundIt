b8b2424d860bd87d558f84de246644af3fdb1814
advaitio <advait.phadnis19@gmail.com>
2026-06-06 15:49:36 +0800

    feat: hide header on home page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "NUSFoundIt" }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="submit-found-item" options={{ title: "Submit Found Item" }} />
       <Stack.Screen name="listings" options={{ title: "Public Listings" }} />
     </Stack>
