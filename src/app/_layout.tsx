b7f0f87aa729436a7c5d37715e92212d48559965
advaitio <advait.phadnis19@gmail.com>
2026-06-11 21:42:15 +0800

    feat: remove temporary screens and related screens for found and lost items

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,11 +5,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={screenOptions}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      // temp screens for direct access during development - will be removed later
-      <Stack.Screen name="submit-found-item" options={{ title: "Submit Found Item", headerBackTitle: "" }} />
-      <Stack.Screen name="found-item-listings" options={{ title: "Public Listings", headerBackTitle: "" }} />
-      <Stack.Screen name="submit-lost-item" options={{ title: "Submit Lost Item", headerBackTitle: "" }} />
-      <Stack.Screen name="lost-item-listings" options={{ title: "Public Listings", headerBackTitle: "" }} />
     </Stack>
   )
 }
