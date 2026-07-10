e960ab996a13499eb3236bd07f15bf34ee077340
kvarg495 <kevinvarghese495@gmail.com>
2026-07-10 18:33:12 +0800

    refactor: removed previously added web containers and styling to focus on mobile web browsers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,11 @@
 import { Stack } from 'expo-router';
-import { Platform, View } from 'react-native';
-import { screenOptions, webLayoutStyles } from '../styles/globalStyles';
+import { screenOptions } from '../styles/globalStyles';
 
 export default function RootLayout() {
-  const content = (
+  return (
     <Stack screenOptions={screenOptions}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="item-details" options={{ title: 'Item Details', headerBackButtonDisplayMode: "minimal" }} />
     </Stack>
   );
-
-  if (Platform.OS === 'web') {
-    return (
-      <View style={webLayoutStyles.webBackground}>
-        <View style={webLayoutStyles.webContainer}>
-          {content}
-        </View>
-      </View>
-    );
-  }
-
-  return content;
 }
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -401,23 +401,3 @@ export const DateStyles = StyleSheet.create({
         marginBottom: 2,
     },
 });
-
-export const webLayoutStyles = StyleSheet.create({
-    webBackground: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        height: "100%",
-    },
-    webContainer: {
-        width: "100%",
-        height: "100%",
-        ...Platform.select({
-            web: {
-                maxWidth: 430,
-                maxHeight: 932,
-            }
-        })
-    }
-});
