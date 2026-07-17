c6d4ed4d866bdc7f855625b6cff87d899287e422
kvarg495 <kevinvarghese495@gmail.com>
2026-07-17 15:11:50 +0800

    feat: update image picker functionality skeleton to improve aesthetics and remove unneccesary cropping edits

--- src/components/FoundItemForm.tsx
+++ src/components/FoundItemForm.tsx
@@ -85,9 +85,7 @@ export default function FoundItemForm() {
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [4, 3],
+            mediaTypes: ["images"],
             quality: 0.5,
         });
 
@@ -189,7 +187,7 @@ export default function FoundItemForm() {
                 placeholderTextColor={colors.placeholder}
                 value={description}
                 onChangeText={(text) => {
-                    const lines = text.split('\n');
+                    const lines = text.split("\n");
                     if (lines.length <= 3) {
                         setDescription(text);
                     }
@@ -204,7 +202,7 @@ export default function FoundItemForm() {
                         Suggestions.dropdownPopover,
                         {
                             backgroundColor: colors.background,
-                            position: 'absolute',
+                            position: "absolute",
                             bottom: 70,
                             marginTop: 0,
                             marginBottom: 0,
@@ -272,8 +270,8 @@ export default function FoundItemForm() {
                             left: 0,
                             right: 0,
                             bottom: 0,
-                            width: '100%',
-                            height: '100%',
+                            width: "100%",
+                            height: "100%",
                             padding: 0,
                             margin: 0,
                         }}
@@ -292,7 +290,7 @@ export default function FoundItemForm() {
                 />
             )}
 
-            {showCalendar && Platform.OS === 'ios' && (
+            {showCalendar && Platform.OS === "ios" && (
                 <Button title="Confirm Date" onPress={() => setShowCalendar(false)} />
             )}
             <TextInput
@@ -314,7 +312,8 @@ export default function FoundItemForm() {
                 maxLength={8}
                 onFocus={() => setShowSuggestions(false)}
             />
-            <Pressable
+            {!image && (
+                <Pressable
                 style={globalStyles.input}
                 onPress={pickImage}
             >
@@ -322,6 +321,7 @@ export default function FoundItemForm() {
                     {image ? "Change Image..." : "Upload Image (optional)"}
                 </Text>
             </Pressable>
+            )}
             {image && (
                 <View style={ImageStyles.imageBox}>
                     <Image source={{uri: image}} style={ImageStyles.image} />
